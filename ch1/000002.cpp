645005187ecb42a6b1300f2f6500be7f71d4a34d
casedecker1101 <renemlopezusd@gmail.com>
2026-07-02 20:23:42 -0700

    Initial Commit

---
+++ ch1/000002.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // declares num as a floating point number variable
+    float num;
+    string purpose;
+
+    // Displays this text to the console
+    cout << "What is my purpose?" << endl;
+
+    // Takes the user's input and stores it in num or string
+    cin >> purpose;
+
+    cout << purpose << "Oh my god..." << endl;
+
+    return 0;
+    
+}
